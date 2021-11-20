3a95497e29489cb3124a5eba2671c2f3bfbb9e04
Tuan Le <letuan317@gmail.com>
2021-11-19 23:19:14 -0800

    update

--- layout.py
+++ layout.py
@@ -4,6 +4,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from termcolor import cprint
 from urllib import request
+import random
 
 program_name = "Youtube Video Download"
 
@@ -14,12 +15,13 @@ class UIApp(QWidget):
         self.window_width = 800
         self.window_height = 500
 
-        self.setGeometry(0, 0, self.window_width, self.window_height)
+        self.setGeometry(80, 50, self.window_width, self.window_height)
         self.setMinimumSize(self.window_width, self.window_height)
         self.setWindowTitle(program_name)
 
         self.colorDarkGreen = "#264653"
         self.colorGreen = "#2a9d8f"
+        self.colorGreenArmy = "#6b705c"
         self.colorYellow = "#e9c46a"
         self.colorOrange = "#f4a261"
         self.colorRed = "#e76f51"
@@ -32,12 +34,10 @@ class UIApp(QWidget):
 
         self.sizeVideoContainer = 100
 
-        self.MenuContainer()
-        self.VideoContainer(x=800,
-                            y=110, url_image='https://live.staticflickr.com/65535/49251422908_591245c64a_c_d.jpg',
-                            title="12345678901234567890123456789012345678901234567890123456789012345678901234567890",
-                            channel="12345678901234567890", formats=["01", "02", "03", "04"], status="Wait")
+        self.videosContainer = list()
 
+        self.MenuContainer()
+        self.ContentContainer()
         self.Footer("Ready")
 
         self.show()
@@ -70,11 +70,36 @@ class UIApp(QWidget):
         self.textSetting = QLabel("Settings", self)
         self.textSetting.setStyleSheet("QWidget { color: white}")
 
+    def ContentContainer(self):
+        self.scroll = QScrollArea(self)
+        self.widget = QWidget(self)
+        self.vbox = QVBoxLayout(self)
+
+        for i in range(0, 5):
+            videoBackground, thumbnail, title, channel, combo_box, status = self.VideoContainer(y=110*(i), url_image='https://live.staticflickr.com/65535/49251422908_591245c64a_c_d.jpg', title="12345678901234567890123456789012345678901234567890123456789012345678901234567890", channel=str(
+                i)+"-12345678901234567890", formats=[str(i), "01", "02", "03", "04"], status=random.choice(["Wait", "Downloading", "Downloaded"]))
+            self.videosContainer.append({"videoBackground": videoBackground, "thumbnail": thumbnail, "title": title,
+                                        "channel": channel, "combo_box": combo_box, "status": status})
+            self.vbox.addWidget(videoBackground)
+            self.vbox.addWidget(thumbnail)
+            self.vbox.addWidget(title)
+            self.vbox.addWidget(channel)
+            self.vbox.addWidget(combo_box)
+            self.vbox.addWidget(status)
+
+        self.widget.setLayout(self.vbox)
+
+        self.scroll.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        self.scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.scroll.setWidgetResizable(True)
+        self.scroll.setWidget(self.widget)
+        self.scroll.setGeometry(
+            0, 100, self.window_width, 5*110)
+
     def resizeEvent(self, event):
         window_size = self.geometry()
         self.window_width = window_size.width()
         self.window_height = window_size.height()
-        cprint((self.window_width, self.window_height), "yellow")
 
         self.menuBackground.setGeometry(
             0, 0, self.window_width, self.sizeMenuContainer)
@@ -83,51 +108,71 @@ class UIApp(QWidget):
         self.textSetting.move(
             self.window_width - self.sizeMenuIcon-17, self.posYMenuIcon+self.sizeMenuIcon+5)
 
+        self.scroll.setGeometry(
+            0, 100, self.window_width, self.window_height-120)
+
         self.footerBackground.setGeometry(
             0, self.window_height-self.sizeFooterContainer, self.window_width, self.sizeFooterContainer)
         self.footerMessage.move(
             5, self.window_height-self.sizeFooterContainer+1)
 
-    def VideoContainer(self, x, y, url_image, title, channel, formats, status):
-        print("test", x)
+        for i in range(0, len(self.videosContainer)):
+            self.videosContainer[i]["videoBackground"].setGeometry(
+                5, 110*(i)+10, self.window_width-10, 100)
+            self.videosContainer[i]["thumbnail"].move(10, 110*(i)+10)
+            self.videosContainer[i]["title"].setGeometry(
+                175, 110*(i)+10, self.window_width-190, 50)
+            self.videosContainer[i]["channel"].setGeometry(
+                175, 110*(i)+60, (self.window_width-180)*2/10, 50)
+            self.videosContainer[i]["combo_box"].setGeometry(
+                175+(self.window_width-180)*2/10+5, 110*(i)+72, (self.window_width-180)*6/10, 25)
+            self.videosContainer[i]["status"].setGeometry(
+                175+(self.window_width-180)*8/10+10, 110*(i)+60, (self.window_width-180)*1.9/10-10, 50)
+
+    def VideoContainer(self, y, url_image, title, channel, formats, status):
         videoBackground = QFrame(self)
         videoBackground.setStyleSheet(
-            "QWidget { border-radius: 5px; background-color: %s}" % self.colorGreen)
-        #videoBackground.setGeometry(0, y, 800, 100)
-        videoBackground.setMinimumSize(QSize(800, 100))
-        videoBackground.setSizePolicy(QSizePolicy.Fixed, 100)
-        videoBackground.move(0, y)
+            "QWidget { border-radius: 5px; background-color: %s}" % self.colorGreenArmy)
+        videoBackground.setGeometry(5, y+10, 800, 100)
 
         data = request.urlopen(url_image).read()
-
         pixmap = QPixmap()
         pixmap.loadFromData(data)
-        #pixmap.scaled(100, 80, Qt.IgnoreAspectRatio, Qt.FastTransformation)
-        pixmap = pixmap.scaledToWidth(100)
-        pixmap = pixmap.scaledToHeight(90)
+        pixmap = pixmap.scaled(200, 90, Qt.KeepAspectRatio)
         thumbnail = QLabel(self)
         thumbnail.setPixmap(pixmap)
-        thumbnail.move(5, y+5)
+        thumbnail.move(10, y+5)
 
         title = QLabel(title, self)
         title.setStyleSheet(
-            "QWidget { color: white; font-size: 15px; font-weight: bold; background: red}")
-        title.setGeometry(175, y + 5, self.window_width-200, 50)
+            "QWidget { color: white; font-size: 15px; font-weight: bold}")
+        title.setGeometry(175, y+10, 600, 50)
 
         channel = QLabel(channel, self)
         channel.setStyleSheet(
             "QWidget { color: white;}")
-        channel.move(175, y + 75)
+        channel.setGeometry(175, y+60, 160, 50)
 
         combo_box = QComboBox(self)
         for ft in formats:
             combo_box.addItem(ft)
-        combo_box.setGeometry(350, y + 75, 300, 20)
+        combo_box.setGeometry(175, y+72, 160, 50)
 
-        channel = QLabel(status, self)
-        channel.setStyleSheet(
-            "QWidget { color: white; font-size: 15px; font-weight: bold}")
-        channel.move(self.window_width-100, y + 75)
+        if status == "Wait":
+            colorStatus = "white"
+        elif status == "Downloading":
+            colorStatus = self.colorYellow
+        elif status == "Downloaded":
+            colorStatus = self.colorGreen
+        else:
+            print(status)
+            colorStatus = self.colorRed
+        status = QLabel(status, self)
+        status.setStyleSheet(
+            "QWidget { color: "+colorStatus+"; font-size: 15px; font-weight: bold;}")
+        status.setGeometry(175, y+60, 100, 50)
+
+        return videoBackground, thumbnail, title, channel, combo_box, status
 
     def AddMenuButton(self, name, text_adjust, path_img, x, y, action):
         btnIconAddfile = QLabel(self)
